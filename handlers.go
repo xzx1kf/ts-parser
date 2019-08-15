1537108697ef267f60673dc3d08ba70d8505fd85
Nick Hale <nicholas.hale@gmail.com>
2019-08-15 14:59:05 +0000

    gofmt applied to handlers.go

--- handlers.go
+++ handlers.go
@@ -48,18 +48,18 @@ func GetBookings(w http.ResponseWriter, r *http.Request) {
 	}
 
 	/*
-	fmt.Println("Available to Rebook")
-	doc.Find(".booking div.cancelled a").Each(func(i int, s *goquery.Selection) {
-		fmt.Println("Call the club to book this court")
-	})
+		fmt.Println("Available to Rebook")
+		doc.Find(".booking div.cancelled a").Each(func(i int, s *goquery.Selection) {
+			fmt.Println("Call the club to book this court")
+		})
 
-	fmt.Println("Booked Courts")
-	doc.Find(".booking div.booked a").Each(func(i int, s *goquery.Selection) {
-		bl, exists := s.Attr("href")
-		if exists {
-			fmt.Println("Booking Link: "+bl+" "+s.Text())
-		}
-	})
+		fmt.Println("Booked Courts")
+		doc.Find(".booking div.booked a").Each(func(i int, s *goquery.Selection) {
+			bl, exists := s.Attr("href")
+			if exists {
+				fmt.Println("Booking Link: "+bl+" "+s.Text())
+			}
+		})
 	*/
 	// TODO: add some logic around if playerA and playerB are known
 }
@@ -72,14 +72,14 @@ func parseBookingUrl(link string) Booking {
 		log.Fatal(err)
 	}
 
-	bs := Booking {
-		Court:		m["court"][0],
-		Days:		m["days"][0],
-		Hour:		m["hour"][0],
-		Min:		m["min"][0],
-		Timeslot:	m["timeSlot"][0],
-		Booked:		false,
-		BookingLink:	"http://tynemouth-squash.herokuapp.com" + link,
+	bs := Booking{
+		Court:       m["court"][0],
+		Days:        m["days"][0],
+		Hour:        m["hour"][0],
+		Min:         m["min"][0],
+		Timeslot:    m["timeSlot"][0],
+		Booked:      false,
+		BookingLink: "http://tynemouth-squash.herokuapp.com" + link,
 	}
 
 	return bs
